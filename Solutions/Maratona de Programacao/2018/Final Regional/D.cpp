ba9c512530198e08515a6d3a8bb4ad32a123c998
VLVentura <vincius.ventura@hotmai.com>
2020-02-20 16:06:49 -0300

    problem D - maratona de programacao 2018

---
+++ Solutions/Maratona de Programacao/2018/Final Regional/D.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+	
+	int N{0}, count{0}, a{0};
+
+	scanf("%d", &N);
+	while(N--){
+		scanf("%d", &a);
+		(a == 1) ? count = count : ++count;
+	}
+	printf("%d", count);
+
+	return 0;
+}
