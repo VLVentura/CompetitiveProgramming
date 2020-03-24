22ab8c22e2b0c7cf5c9cb222b24da34814ac1967
VLVentura <vincius.ventura@hotmai.com>
2020-03-24 15:06:05 -0300

    add UVa solutions

--- Solutions/UVa Online Judge/10114 - Loansome Car Buyer.cpp
+++ Solutions/UVa Online Judge/10114 - Loansome Car Buyer.cpp
@@ -16,11 +16,11 @@ int main(){
 		}
 
 		int index{0}, time{0};
-		double owes{B}, carValue{(B+P)-(B+P)*historical[index].second}, valueMonth{B/M};
+		double owes{B}, carValue{(B+P)-(B+P)*historical[index].second}, valuePerMonth{B/M};
 		while(time < M  && owes >= carValue){
 			++time;
 			if(index < historical.size() - 1) time == historical[index + 1].first ? ++index : index = index;
-			owes -= valueMonth;
+			owes -= valuePerMonth;
 			carValue -= carValue*historical[index].second;
 		}
 		printf("%d ", time);
