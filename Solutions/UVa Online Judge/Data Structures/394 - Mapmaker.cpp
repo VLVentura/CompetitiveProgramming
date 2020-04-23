e54d6bae83a12315f0ee158094d72b6a0adc7385
VLVentura <vincius.ventura@hotmai.com>
2020-04-23 11:39:18 -0300

    updating

---
+++ Solutions/UVa Online Judge/Data Structures/394 - Mapmaker.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define deb(x) cout << #x << " " << x << "\n";
+
+#define all(x) x.begin(), x.end()
+#define pb push_back
+#define eb emplace_back
+#define fi first
+#define snd second
+
+#define vi vector<int>
+#define vii vector<vi>
+
+int main(){
+	
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);	
+	
+	
+	
+	return 0;
+}
