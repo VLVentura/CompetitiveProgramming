4d081722ef6d3a235a3604cce7e58167b266189f
VLVentura <vincius.ventura@hotmai.com>
2020-04-24 15:02:45 -0300

    add UVa solutions

---
+++ Solutions/UVa Online Judge/Data Structures/12150 - Pole Position.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define deb(x) cout << #x << " " << x << "\n";
+
+#define all(x) x.begin(), x.end()
+#define fi first
+#define snd second
+
+int main(){
+	
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);	
+	
+
+
+	return 0;
+}
